2e3e149ce9d8688ccab3252492618c1f4c37ef6e
unknown <rahulsurana37@gmail.com>
2020-06-12 22:00:48 +0530

    Loop for Count added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,13 +2,32 @@
 
 echo "Welcome to Flip-Coin Simulation Code"
 
+flipping=10;
+a=0;
 heads=0;
 tails=1;
+h_count=0;
+t_count=0;
 
-flip=$(($RANDOM%2))
+while [[ $a -le $flipping ]]; do
+	flip=$(($RANDOM%2))
+	if [[ $flip -eq heads ]]; then
+		h_count=$(($h_count+1))
+		
+	else
+		t_count=$(($t_count+1));
+		
+	fi
+	a=$(($a+1))
+done
 
-if [[ $flip -eq heads ]]; then
-	echo "Winner is Heads"
+echo "Heads Count is " $h_count
+echo "Tails Count is" $t_count
+
+if [[ $h_count -ge $t_count ]]; then
+	echo "Heads is the Winner."
+elif [[ $t_count -ge $h_count ]]; then
+	echo "Tails is the winner"
 else
-	echo "Winner is Tails"
+	echo "It's a draw"
 fi
