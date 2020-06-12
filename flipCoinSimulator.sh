a42bcac8324b8fd04e72fc5acea1369387bbd144
unknown <rahulsurana37@gmail.com>
2020-06-12 21:44:06 +0530

    Added Winners

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Welcome to Flip-Coin Simulation Code"
+
+heads=0;
+tails=1;
+
+flip=$(($RANDOM%2))
+
+if [[ $flip -eq heads ]]; then
+	echo "Winner is Heads"
+else
+	echo "Winner is Tails"
+fi
